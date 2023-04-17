8b159b45ef1cdb6c1e0f8349740ca29146e78ea0
BarnesNoble <anna.mitchell.dev@gmail.com>
2023-04-17 12:53:21 -0400

    Added comments and cancel spell check for input value

--- src/script.js
+++ src/script.js
@@ -5,12 +5,19 @@ const api = {
 const input = document.querySelector("#inputField");
 input.addEventListener("keydown", enter);
 
+//* Function to get info from input field *//
 function enter(event){
 	if (event.keyCode === 13) {
 		getInfo(input.value);
 	}
 }
+//* Function to get results from API *//
 async function getInfo(data){
 	const getInfoResult = await fetch(`${api.endpoint}weather?q=${data}&units=imperial&appID=${api.key}`);
-	console.log(getInfoResult);
+	const weatherResult = await getInfoResult.json();
+	displayResult(weatherResult);
+}
+//* Function to show info on the screen *//
+function displayResult(weatherResult){
+	console.log(weatherResult);
 }
